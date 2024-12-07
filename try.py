c416493b7acc804c05b77892849ed1afeed12661
Udisha Verma <udishaverma13@gmail.com>
2024-12-07 19:13:47 +0530

    first commit

--- try.py
+++
@@ -1,8 +0,0 @@
-#find factors of a given number n
-
-n=int(input("Enter n: "))
-factors=[]
-for i in range(1,n+1):
-    if n%i==0:
-        factors.append(i)
-print(f"The factors of {n} are {factors}")
